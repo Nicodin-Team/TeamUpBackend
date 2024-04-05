dbb9872aed4bddc3731d213adb7074f63048497d
unknown <aidinzaeim@gmail.com>
2024-04-05 10:23:51 -0700

    Users of one project API

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('accounts.urls')),    
+    path('projects/', include('projects.urls')),    
 ]
