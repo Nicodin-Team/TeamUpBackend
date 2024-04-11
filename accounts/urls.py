c9ce382f99258dee1a331ea0bc303ad0b1dbf19e
unknown <aidinzaeim@gmail.com>
2024-04-11 03:07:53 -0700

    pagination update

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('get_token/', PasswordResetAPIView.as_view(), name='get_token'),
     path('recover_password/', PasswordRecoveryAPIView.as_view(), name='password_recovery'),
     path('users/<int:pk>', GetUserAPIView.as_view(), name='get_user'),
-    path('users/all/<int:page_num>', GetUsersListAPIView.as_view(), name='list_users'),
+    path('users/all/', GetUsersListAPIView.as_view(), name='list_users'),
     path('users/delete/', DeleteUserAPIView.as_view(), name='get_user'),
     path('users/update/', UpdateUserAPIView.as_view(), name='update_user'),
     path('skills/', SkillsAPIViews.as_view(), name='skills'),
-    path('skills/delete/', DeleteSkillAPIView.as_view(), name='delete_skill'),
+    path('skills/delete/<int:skill_id>', DeleteSkillAPIView.as_view(), name='delete_skill'),
 ]
