ebbcb6f57cbd58757cbcc30e60ed41796e32d98d
unknown <aidinzaeim@gmail.com>
2024-04-24 12:52:06 -0700

    cors again

--- core/settings.py
+++ core/settings.py
@@ -36,19 +36,21 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 
 ROOT_URLCONF = 'core.urls'
 CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL=True
+ALLOWED_HOSTS = ["*",]
 
 TEMPLATES = [
     {
