a751c13ae00c48ffdfd5d80faf5e083c372afa51
Aidin Asl Zaeim <140266369+AidinZaeim@users.noreply.github.com>
2024-04-30 04:29:42 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DOMAIN = "http://localhost:8000"
-print(BASE_DIR)
+
 
 
 # Quick-start development settings - unsuitable for production
