2f5a8f1d61f90077cf0ce5328f7266d7a0028e44
Mobina Esmaeilpour <m_esmaeilpour@comp.iust.ac.ir>
2024-05-21 23:37:34 +0330

    create app and api blog

---
+++ blog/apis/blog.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from blog.models import Blog, Comment, SubContent, Tag
+from blog.serializers import BlogSerializer, BlogDetailSerializer, CommentSerializer
+
+
+class BlogListAPIView(generics.ListAPIView):
+    #  http://127.0.0.1:8000/blog/list/
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+
+class BlogDetailAPIView(generics.ListAPIView):
+    #  http://127.0.0.1:8000/blog/detail/<int:blog_id>/
+    queryset = Blog.objects.all()
+    serializer_class = BlogDetailSerializer
+
+
+class CommentPostAPIView(generics.CreateAPIView):
+    #  http://127.0.0.1:8000/blog/comment_create/
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
