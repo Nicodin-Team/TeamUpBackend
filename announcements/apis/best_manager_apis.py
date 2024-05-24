752fec0435fa32e07ca3b18f406cb639a2e2f90f
Mobina Esmaeilpour <m_esmaeilpour@comp.iust.ac.ir>
2024-05-24 14:35:21 +0330

    AddScoreView

--- announcements/apis/best_manager_apis.py
+++ announcements/apis/best_manager_apis.py
@@ -1,10 +1,9 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from announcements.models import Manager
-from announcements.serializers import ManagerSerializer
-from rest_framework.permissions import IsAuthenticated
 from django.db.models import Avg
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from announcements.models import Manager, Score
+from announcements.serializers import ManagerSerializer, ScoreSerializer
 
 class ManagerListView(APIView):
     permission_classes = [IsAuthenticated]
@@ -22,3 +21,13 @@ class BestManagerView(APIView):
         best_manager = max(managers, key=lambda x: x.scores.all().aggregate(avg_score=Avg('value'))['avg_score'])
         serializer = ManagerSerializer(best_manager)
         return Response(serializer.data)
+    
+
+class AddScoreView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        manager = Manager.objects.get(user=request.user)
+        score = Score.objects.create(manager=manager, value=request.data['value'])
+        serializer = ScoreSerializer(score)
+        return Response(serializer.data)    
--- announcements/models.py
+++ announcements/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 class Manager(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(CustomUser , on_delete=models.CASCADE, related_name="manager")
-    
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="manager", limit_choices_to={'is_manager': True})
+
 class Announcement(models.Model):
-    user =  models.ForeignKey(CustomUser, related_name='announcements', on_delete=models.CASCADE, null = True)
+    user = models.ForeignKey(CustomUser, related_name='announcements', on_delete=models.CASCADE, null=True, limit_choices_to={'is_manager': True})
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateField(auto_now_add = True,blank=True, null=True)
