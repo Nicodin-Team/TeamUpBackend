d1383df499ec3cd3621e5daa3b075582c2fc26c7
Mobina Esmaeilpour <m_esmaeilpour@comp.iust.ac.ir>
2024-05-24 14:12:03 +0330

    testing the accept/reject by manager

--- announcements/models.py
+++ announcements/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from accounts.models import CustomUser
 from django.db import models
-from django_countries.fields import CountryField
 from django.utils import timezone
 from django.utils.text import slugify
 from django.core.exceptions import ValidationError
