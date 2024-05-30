ef9b9e85ed77cff9e94dd293c8dd3ce3cac1a3a0
unknown <aidinzaeim@gmail.com>
2024-05-30 06:53:40 -0700

    id added for user in response

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -21,8 +21,9 @@ class UserSerializer(serializers.ModelSerializer):
     photo = serializers.ImageField(required=False)
     class Meta:
         model = CustomUser
-        fields = ['photo', 'bio', 'username', 'first_name', 'last_name', 'created_at', 'gender', 'age', 'country', 'city']
-
+        fields = ['id', 'photo', 'bio', 'username', 'first_name', 'last_name', 'created_at', 'gender', 'age', 'country', 'city']
+        read_only_fields = ['id']
+    
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
