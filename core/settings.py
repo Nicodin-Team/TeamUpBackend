3c720afae1f8e5bf37ac8d7b3e3ad374aed3f826
unknown <aidinzaeim@gmail.com>
2024-06-07 12:35:12 +0330

    chat

--- core/settings.py
+++ core/settings.py
@@ -91,17 +91,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 ASGI_APPLICATION = 'core.asgi.application'
 
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+if deploy:
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                "hosts": [('teamupredis', 6379)],
+            },
         },
-    },
-}
-
-
+    }
+else:
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                "hosts": [("redis://:RXF7isG4QTjpmcU6PeT2ZiuK@teamupredis:6379/0")],
+            },
+        },
+    }
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
